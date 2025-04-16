1d724ad69131f11e95debc8d01220c4598d3ceea
류수연 <rsy7567@gmail.com>
2025-04-16 09:48:06 +0900

    [Bronze V] Title: A+B - 3, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,22 +1,22 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder builder = new StringBuilder();
 
         int length = Integer.parseInt(bufferedReader.readLine());
 
         for (int i = 0; i < length; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-            int a = Integer.parseInt(stringTokenizer.nextToken());
-            int b = Integer.parseInt(stringTokenizer.nextToken());
-            bufferedWriter.write(String.valueOf(a + b));
-            bufferedWriter.newLine();
+            StringTokenizer token = new StringTokenizer(bufferedReader.readLine(), " ");
+            int a = Integer.parseInt(token.nextToken());
+            int b = Integer.parseInt(token.nextToken());
+            builder.append(a + b).append("\n");
         }
-        bufferedWriter.flush();
-        bufferedWriter.close();
+        System.out.println(builder);
         bufferedReader.close();
     }
 }
