6f1bb86e17b871d7bc738f5bfbc7e802d226fd35
류수연 <rsy7567@gmail.com>
2025-04-28 10:20:44 +0900

    [Bronze V] Title: 이상한 기호, Time: 104 ms, Memory: 14112 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder output = new StringBuilder();
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(token.nextToken());
+        int B = Integer.parseInt(token.nextToken());
+
+        output.append((A + B) * (A - B));
+
+        System.out.println(output);
+    }
+}
