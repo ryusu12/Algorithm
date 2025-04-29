0d3e570fe81d4a6039c77474a7d486242cb12574
류수연 <rsy7567@gmail.com>
2025-04-29 10:40:56 +0900

    [Bronze III] Title: 웰컴 키트, Time: 356 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,17 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder output = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer token = new StringTokenizer(br.readLine());
-        
+
         StringTokenizer token2 = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(token2.nextToken());
         int P = Integer.parseInt(token2.nextToken());
@@ -24,8 +22,10 @@ public class Main {
                 sum += 1;
             }
         }
-        output.append(sum).append('\n');
-        output.append(N / P).append(" ").append(N % P);
-        System.out.println(output);
+        bw.write(sum + "\n");
+        bw.write(N / P + " " + (N % P));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
