e1dbfcb09beeafca189e1f53545f5b0883770cc1
류수연 <rsy7567@gmail.com>
2025-06-10 09:10:56 +0900

    [Silver V] Title: 수들의 합 5, Time: 148 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 1;
+        int count = 1;
+        int start = 1;
+        int end = 1;
+
+        while (end < N) {
+            if (sum < N) {
+                sum += ++end;
+            } else if (sum == N) {
+                sum += ++end;
+                count++;
+            } else {
+                sum -= start++;
+            }
+        }
+        System.out.println(count);
+    }
+}
