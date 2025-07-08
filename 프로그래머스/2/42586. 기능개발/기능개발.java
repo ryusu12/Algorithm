9be910faabc4c5deadc841d5e03fc3a1d91d7aa7
류수연 <rsy7567@gmail.com>
2025-07-08 09:31:45 +0900

    [level 2] Title: 기능개발, Time: 8.89 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,38 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        boolean[] done = new boolean[progresses.length];
+        Queue<Integer> que = new LinkedList<>();
         List<Integer> finish = new ArrayList<>();
-        int start = 0;
-
-        // done이 모두 true일 때 까지
-        while(start < progresses.length) {
-            // progresses 진행도 갱신
-            int endIdx = -1;
-            for (int i = start; i < progresses.length; i++) {
-                progresses[i] += speeds[i];
-                if (progresses[i] >= 100) {
-                    done[i] = true;
-                    endIdx = i;
-                }
-            }
-
-            // done[0~i] 가 true 이면
-            boolean flag = false;
-            for (int i = start; i <= endIdx; i++) {
-                if (!done[i]) {
-                    endIdx = i - 1;
-                    break;
-                }
-                flag = true;
-            }
-            if (flag) {
-                finish.add(endIdx - start + 1);
-                start = endIdx + 1;
-            }
+        // 몇일 걸리는지 확인
+        for (int i = 0; i < progresses.length; i++) {
+            int day = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            que.add(day);
         }
-        // 완료된 기능 개수 파악 후 저장
-        // answer[idx++]
+        while (!que.isEmpty()) {
+            int first = que.poll();
+            int count = 1;
+            while (!que.isEmpty() && first >= que.element()) {
+                que.remove();
+                count++;
+            }
+            finish.add(count);
+        }
         int[] answer = new int[finish.size()];
         for (int i = 0; i < finish.size(); i++) answer[i] = finish.get(i);
         return answer;
