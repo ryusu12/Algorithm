84f3453709decc4cd37b0d4957f58e19aed36821
류수연 <rsy7567@gmail.com>
2025-12-10 14:00:37 +0900

    [level 1] Title: 폰켓몬, Time: 5.64 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        // map<번호, 마릿수>
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0));
+        }
+        if (map.size() >= nums.length / 2) {
+            return nums.length / 2;
+        } else {
+            return map.size();
+        }
+    }
+}
