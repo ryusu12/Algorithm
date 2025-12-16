4ae5ac56e2ee0578da49a042c1dd04448db26f50
류수연 <rsy7567@gmail.com>
2025-12-16 14:22:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 300.52 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,26 +2,17 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        // completion를 탐색했는지 아닌지 => map<이름, 인원수>
-        Map<String, Integer> check = new HashMap<>();
-        for (int i = 0; i < completion.length; i++) {
-            int num = check.getOrDefault(completion[i], 0);
-            check.put(completion[i], num + 1);
-        }
+        // 정렬
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         
-        for (int i = 0; i < participant.length; i++) {
-            int num = check.getOrDefault(participant[i], 0);
-            // 있으면 인원수-1
-            if (num > 0) {
-                check.put(participant[i], num - 1);
-            } else {
-                // 없으면(인원수 0) return
-                answer = participant[i];
-                break;
+        for (int i = 0; i < completion.length; i++) {
+            // 틀리면 participant가 완주 못함
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
             }
-            
         }
-        return answer;
+        // 차이없으면 마지막 남은 사람이 완주 못함
+        return participant[participant.length - 1];
     }
 }
