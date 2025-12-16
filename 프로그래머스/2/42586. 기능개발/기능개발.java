0aff3afc561685bbfaf58836017840612a280673
류수연 <rsy7567@gmail.com>
2025-12-16 13:31:15 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,24 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> que = new LinkedList<>();
-        List<Integer> finish = new ArrayList<>();
-        // 몇일 걸리는지 확인
+        // 각 작업일 배열
+        int[] last = new int[progresses.length];
         for (int i = 0; i < progresses.length; i++) {
-            int day = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            que.add(day);
-        }
-        while (!que.isEmpty()) {
-            int first = que.poll();
-            int count = 1;
-            while (!que.isEmpty() && first >= que.element()) {
-                que.remove();
-                count++;
+            if ((100 - progresses[i]) % speeds[i] > 0) {
+                last[i] = (100 - progresses[i]) / speeds[i] + 1;
+            } else {
+                last[i] = (100 - progresses[i]) / speeds[i];
             }
-            finish.add(count);
         }
-        int[] answer = new int[finish.size()];
-        for (int i = 0; i < finish.size(); i++) answer[i] = finish.get(i);
+        
+        // 이전에 나간 값이 더 크면 당일배포수++
+        List<Integer> list = new ArrayList<>();
+        int before = last[0];
+        int out = 1;
+        for (int i = 1; i < last.length; i++) {
+            if (before >= last[i]) {
+                out++;
+            }
+            else {
+                list.add(out);
+                out = 1;
+                before = last[i];
+            }
+        }
+        list.add(out);
+        
+        // List<Integer> -> int[]
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
