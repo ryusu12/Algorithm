754161d270218023ecdfa07db236cf45687b07cc
류수연 <rsy7567@gmail.com>
2025-12-17 13:06:23 +0900

    [level 2] Title: 프로세스, Time: 1.29 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 2.74 ms
+메모리: 79.7 MB, 시간: 1.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 09일 09:41:50
+2025년 12월 17일 13:06:21
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,38 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> priorityQue = new LinkedList<>();
-        Queue<Integer> idxQue = new LinkedList<>();
-
-        for (int i = 0; i < priorities.length; i++) {
-            priorityQue.add(priorities[i]);
-            idxQue.add(i);
-        }
-
         int answer = 0;
-
-        while (!priorityQue.isEmpty()) {
-            // 1번
-            int priority = priorityQue.poll();
-            int idx = idxQue.poll();
-            // 2번
-            boolean flag = true;
-            for (int p : priorityQue) {
-                if (priority < p) {
-                    flag = false;
-                    break;
+        Queue<Integer> que = new LinkedList<>();
+        for (int p : priorities) {
+            que.offer(p);
+        }
+        
+        // 최대값을 구하는 배열 -> pop 빠질때마다 idx--
+        Arrays.sort(priorities);
+        int idx = priorities.length - 1;
+        
+        // 큐가 비어있을때까지 순환
+        while (!que.isEmpty()) {
+            // 맨앞 >= 큐의 최대값 ? pop + answer++ : pop + push
+            // 이동할때마다 location 갱신
+            if (que.peek() >= priorities[idx]) {
+                if (location == 0) {
+                    return answer + 1;
+                } else {
+                    location--;
                 }
-            }
-            // 3번
-            if (flag) {
+                que.poll();
                 answer++;
-                if (idx == location) return answer;
+                idx--;
             } else {
-                priorityQue.add(priority);
-                idxQue.add(idx);
+                if (location == 0) {
+                    location = que.size() - 1;
+                } else {
+                    location--;
+                }
+                que.offer(que.poll());
             }
         }
         return answer;
     }
-
 }
