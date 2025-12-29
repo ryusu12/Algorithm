8d576a08b2addddc4633d20bbac430151ab0e104
류수연 <rsy7567@gmail.com>
2025-12-29 14:41:17 +0900

    [level 1] Title: 체육복, Time: 0.21 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 1.28 ms
+메모리: 85.1 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 30일 09:50:27
+2025년 12월 29일 14:41:08
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,23 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> reserveList = new ArrayList<>();
+        int answer = n;
+        
         List<Integer> lostList = new ArrayList<>();
-        for (int i : reserve) reserveList.add(i);
-
-        for (int los : lost) {
-            if (!reserveList.remove(Integer.valueOf(los))) lostList.add(los);
+        List<Integer> reserveList = new ArrayList<>();
+        for (int r : reserve) reserveList.add(r);
+        
+        // 사전 처리 : 여벌 있는 학생이 도난당한 경우 - 빌려줄 수 없음
+        for (int lst : lost) {
+            int idx = reserveList.indexOf(lst);
+            if (idx != -1) {
+                reserveList.remove(idx);
+            } else {
+                lostList.add(lst);
+            }
         }
         
-        reserveList.sort((a, b) -> a - b);
-        lostList.sort((a, b) -> a - b);
-
-        int lostNum = lostList.size();
-        for (int los : lostList) {
-            if (reserveList.remove(Integer.valueOf(los))) lostNum--;
-            else if (reserveList.remove(Integer.valueOf(los - 1))) lostNum--;
-            else if (reserveList.remove(Integer.valueOf(los + 1))) lostNum--;
+        // 체격순으로 정렬하기
+        Collections.sort(reserveList);
+        Collections.sort(lostList);
+        
+        // lost의 전후 값이 reserve에 있는지? 전부터 검사
+        for (int lst : lostList) {
+            int frontIdx = reserveList.indexOf(lst - 1);
+            int backIdx = reserveList.indexOf(lst + 1);
+            if (frontIdx != -1) {
+                reserveList.remove(frontIdx);
+                continue;
+            }
+            else if (backIdx != -1) {
+                reserveList.remove(backIdx);
+                continue;
+            } else {
+                answer--;
+            }
         }
-        return n - lostNum;
+        
+        return answer;
     }
 }
